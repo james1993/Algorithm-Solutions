25a9e4bd78b6550bf9d3791ae906d2ce470fb2fc
James Ogletree <jamesogletree@gmail.com>
2021-04-30 14:16:29 -0500

    Time: 40 ms (33.76%), Space: 32.7 MB (44.91%) - LeetHub

--- merge-two-binary-trees/merge-two-binary-trees.cpp
+++ merge-two-binary-trees/merge-two-binary-trees.cpp
@@ -12,17 +12,27 @@
 class Solution {
 public:
     
+    void recurse(TreeNode* node1, TreeNode* node2, TreeNode*& new_tree)
+    {
+        if(!node1 && !node2) 
+            new_tree = NULL;
+        else if(node1 && !node2)
+            new_tree = node1;
+        else if(node2 && !node1)
+            new_tree = node2;
+        else
+        {
+            TreeNode* new_node = new TreeNode(node1->val + node2->val);
+            new_tree = new_node;
+        
+            recurse(node1->left, node2->left, new_tree->left);
+            recurse(node1->right, node2->right, new_tree->right);
+        }
+    }
+    
     TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
-        if(!root1 && !root2) return NULL;
-        if(!root1) return root2;
-        if(!root2) return root1;
-        
-        root1->val += root2->val;
-        
-        root1->left = mergeTrees(root1->left, root2->left);
-        root1->right = mergeTrees(root1->right, root2->right);
-        
-        return root1;
-        
+        TreeNode* new_tree = new TreeNode();
+        recurse(root1, root2, new_tree);
+        return new_tree;
     }
 };
