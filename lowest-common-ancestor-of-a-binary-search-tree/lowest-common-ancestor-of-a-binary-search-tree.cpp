5aa7fb68b043d7a581713853d3bf85c2fd4f0449
James Ogletree <jamesogletree@gmail.com>
2021-04-30 14:36:33 -0500

    Time: 20 ms (98.96%), Space: 23.3 MB (35.75%) - LeetHub

--- lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -10,13 +10,38 @@
 
 class Solution {
 public:
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(p->val < root->val && q->val < root->val)
-            return lowestCommonAncestor(root->left, p, q);
+    void recurse(TreeNode* node, TreeNode* p, TreeNode* q, TreeNode*& lca)
+    {
+        if(!node) return;
         
-        if(p->val > root->val && q->val > root->val)
-            return lowestCommonAncestor(root->right, p, q);
+        if(p->val <= node->val && q->val >= node->val && !lca) 
+            lca = node;
+        else
+        {
+            recurse(node->left, p, q, lca);
+            recurse(node->right, p, q, lca);
+        }
+    }
+    
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) 
+    {
+        TreeNode* lca = NULL;
+        TreeNode* lesser = NULL;
+        TreeNode* greater = NULL;
         
-        return root;
+        
+        if(p->val < q->val)
+        {
+            lesser = p;
+            greater = q;
+        }
+        else
+        {
+            lesser = q;
+            greater = p;
+        }
+        
+        recurse(root, lesser, greater, lca);
+        return lca;
     }
 };
