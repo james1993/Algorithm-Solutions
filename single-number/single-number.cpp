14e5a0d1980239306b53173052fe4e1d991e4f81
James Ogletree <jamesogletree@gmail.com>
2021-04-30 19:19:52 -0500

    Time: 16 ms (82.99%), Space: 17 MB (43.77%) - LeetHub

---
+++ single-number/single-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int x1 = 0;
+        
+        for(int i = 0; i < nums.size(); i++)
+        {
+            x1 ^= nums[i];
+        }
+        
+        return x1;
+    }
+};
