afe73975be32e0c15c4edcc3d9c6038af7de9107
James Ogletree <jamesogletree@gmail.com>
2021-05-14 19:23:08 -0500

    Time: 12 ms (24.90%), Space: 10.2 MB (6.68%) - LeetHub

--- two-sum/two-sum.cpp
+++ two-sum/two-sum.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int> compliments;
-        vector<int> indices(2);
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        unordered_map<int, int> map;
+        vector<int> indices;
         for(int i = 0; i < nums.size(); i++)
         {
-            auto iter = compliments.find(target-nums[i]);
-            if(iter == compliments.end())
-                compliments.insert({nums[i],i});
+            if(map.find(target - nums[i]) == map.end())
+            {
+                map.insert({nums[i], i});
+            }
             else
             {
-                indices[0] = (*iter).second;
-                indices[1] = i;
-                break;
+                indices = {i, map.find(target - nums[i])->second};
             }
         }
-        
         return indices;
     }
 };
