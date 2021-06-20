b9c7578c38b97fd57e1656765a7439552e3b7d8a
James Ogletree <jamesogletree@gmail.com>
2021-06-20 18:47:16 -0500

    Time: 8 ms (52.74%), Space: 13.2 MB (52.93%) - LeetHub

--- maximum-subarray/maximum-subarray.cpp
+++ maximum-subarray/maximum-subarray.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        if(nums.size() == 1) return nums[0];
-
-        int running_sum = 0;
-        int max_sum = numeric_limits<int>::min();
-        for(int i = 0; i < nums.size(); i++)
+        if(nums.size() == 0) return 0;
+        int running_sum = nums[0];
+        int max_sum = nums[0];
+        for(int i = 1; i < nums.size(); i++)
         {
             running_sum += nums[i];
             if(nums[i] > running_sum) running_sum = nums[i];
-            max_sum = max(running_sum, max_sum);
+            max_sum = max(max_sum, running_sum);
         }
         return max_sum;
     }
