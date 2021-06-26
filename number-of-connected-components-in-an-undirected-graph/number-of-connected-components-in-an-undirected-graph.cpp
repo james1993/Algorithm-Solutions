291723d01d37bec28b41e9694f43ef98477641aa
James Ogletree <jamesogletree@gmail.com>
2021-06-26 12:25:41 -0500

    Time: 24 ms (55.68%), Space: 13.8 MB (47.80%) - LeetHub

--- number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.cpp
+++ number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     void dfs(vector<int> adjList[], vector<int> &visited, int src) {
         visited[src] = 1;
         
@@ -9,23 +10,46 @@ public:
             }
         }
     }
-    
+    */
     int countComponents(int n, vector<vector<int>>& edges) {
         if (n == 0) return 0;
       
         int components = 0;
         vector<int> visited(n, 0);
-        vector<int> adjList[n];
+        vector<vector<int>> adjList(n);
     
         for (int i = 0; i < edges.size(); i++) {
             adjList[edges[i][0]].push_back(edges[i][1]);
             adjList[edges[i][1]].push_back(edges[i][0]);
         }
         
-        for (int i = 0; i < n; i++) {
-            if (visited[i] == 0) {
+        for (int i = 0; i < n; i++) 
+        {
+            if (visited[i] == 0) 
+            {
                 components++;
-                dfs(adjList, visited, i);
+                visited[i] = 1;
+                
+                queue<int> queue;
+                queue.push(i);
+                while(!queue.empty())
+                {
+                    int level_size = queue.size();
+                    for(int i = 0; i < level_size; i++)
+                    {
+                        int current = queue.front();
+                        queue.pop();
+                        
+                        for(int j = 0; j < adjList[current].size(); j++)
+                        {
+                            if(visited[adjList[current][j]] == 0)
+                            {
+                                visited[adjList[current][j]] = 1;
+                                queue.push(adjList[current][j]);
+                            }
+                        }
+                    }
+                }
             }
         }
         return components;
